4c30673071788606e74ea2d4aa5cb4d1258479cf
Felix Santiago <fsantiago@hearsaycorp.com>
2013-10-04 21:20:20 -0700

    Initial project commit

---
+++ TemperatureConverter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TemperatureConverter
+//
+//  Created by Felix Santiago on 10/4/13.
+//  Copyright (c) 2013 Felix Santiago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TemperatureConverter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TemperatureConverter
+//
+//  Created by Felix Santiago on 10/4/13.
+//  Copyright (c) 2013 Felix Santiago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
